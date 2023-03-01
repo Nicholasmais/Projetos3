8e3d541a0b6808be9cac872a404e4ec09daa0049
zeranic <nicholasfonseca@fitec.org.br>
2023-03-01 17:05:27 -0300

    Tabela de pessoas

--- database_handler.py
+++ database_handler.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import configparser
+from datetime import datetime
 
 class DatabaseHandler():
     def __init__(self):
@@ -37,8 +38,7 @@ class DatabaseHandler():
         cursor.execute(query)
         return cursor.fetchall()
     
-    def get_table_columns(self):
-        
+    def get_table_columns(self):    
         query = f"select * from pessoas;"
         cursor = self.db.cursor()
         cursor.execute(query)
@@ -47,7 +47,7 @@ class DatabaseHandler():
         for row in res:
             pessoas[row[0]] = row[1]
 
-        query = f"select apartamento.apartamento, pessoas.nome from apartamento inner join pessoas on apartamento.codigo = pessoas.apartamento;"
+        query = f"select apartamento.apartamento, pessoas.nome from apartamento left join pessoas on apartamento.codigo = pessoas.apartamento;"
         cursor = self.db.cursor()
         cursor.execute(query)
         res = cursor.fetchall()
@@ -59,12 +59,41 @@ class DatabaseHandler():
             else:
                 pessoas_info[row[0]].append(row[1])
 
-        query = f"select apartamento.*, placas_cadastradas.placa from apartamento inner join placas_cadastradas on apartamento.responsavel = placas_cadastradas.responsavel;"
+        query = f"select apartamento.codigo, apartamento.responsavel, placas_cadastradas.placa from apartamento left join placas_cadastradas on apartamento.responsavel = placas_cadastradas.responsavel order by codigo;"
         cursor = self.db.cursor()
         cursor.execute(query)
         res = cursor.fetchall()
         apartaments_info = {}
         for row in res:   
-            apartaments_info[row[1]] = {"responsavel":pessoas[row[2]],"moradores":pessoas_info[row[1]], "placa":row[3]}
+            apartaments_info[row[0]] = {"responsavel":pessoas.get(row[1],""),"moradores":pessoas_info.get(row[0],[]), "placa":row[2] if row[2] else ''}
         return apartaments_info
-    #apartemtno responsavel placa
+
+    def get_pessoas_columns(self):
+        query = f"select * from pessoas;"
+        cursor = self.db.cursor()
+        cursor.execute(query)
+        res = cursor.fetchall()
+        pessoas = {}
+        for row in res:
+            pessoas[row[1]] = {"apartamento":row[2], "data_nascimento":datetime.strptime(str(row[3]), "%Y-%m-%d").strftime('%d/%m/%Y'), "tipo_pessoa":row[4]}
+        return pessoas
+
+    def update_apartament(self, apto, responsavel):
+        query = f"SELECT nome, codigo from pessoas"
+        cursor = self.db.cursor()
+        cursor.execute(query)
+        res = cursor.fetchall()
+        responsvael_nome = {responsavel:codigo for responsavel,codigo in res}
+     
+        query = 'update apartamento set responsavel = %s where apartamento = %s'
+        parameters = (responsvael_nome[responsavel], apto)
+        cursor = self.db.cursor()
+        cursor.execute(query, parameters)
+   
+        self.db.commit()
+
+    def create_pessoa(self, *args):
+        query = 'insert into pessoas(nome, apartamento, data_nascimento, tipo_pessoa) values (%s, %s, %s, %s)'
+        val = args
+        self.insert(query, val)
+        return None
--- gate_keeper.py
+++ gate_keeper.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import tkinter as tk
 from tkinter import ttk
 import tkcalendar 
@@ -24,27 +25,40 @@ frame_bottom = tk.Frame(janela, width=int(janel_width*.8), height=janela_height/
 frame_bottom.place(x=int(janel_width*.1), y=janela_height/2)
 frame_bottom.update()
 
-frame_inputs = tk.Frame(frame_bottom, bd=5, relief='ridge')
-frame_inputs.place(relx=0, rely=0, relwidth=0.25, relheight=1)
+frame_bottom_left = tk.Frame(frame_bottom)
+frame_bottom_left.place(relx=0, rely=0, relwidth=.2, relheight=1)
+frame_bottom_left.update()
+
+frame_bottom_right = tk.Frame(frame_bottom)
+frame_bottom_right.place(relx=.2, rely=0, relwidth=.8, relheight=1)
+frame_bottom_right.update()
+
+frame_bottom_right_left_top = tk.Frame(frame_bottom_right)
+frame_bottom_right_left_top.place(relx=0, rely=0, relwidth=.5, relheight=.3)
+frame_bottom_right_left_top.update()
+
+frame_bottom_right_left_bottom = tk.Frame(frame_bottom_right)
+frame_bottom_right_left_bottom.place(relx=0, rely=.3, relwidth=.5, relheight=.7)
+frame_bottom_right_left_bottom.update()
+
+frame_bottom_right_right_top = tk.Frame(frame_bottom_right, bd=5, relief='ridge')
+frame_bottom_right_right_top.place(relx=.5, rely=0, relwidth=.5, relheight=.3)
+frame_bottom_right_right_top.update()
+
+frame_bottom_right_right_bottom = tk.Frame(frame_bottom_right)
+frame_bottom_right_right_bottom.place(relx=.5, rely=.3, relwidth=.5, relheight=.7)
+frame_bottom_right_right_bottom.update()
+
+frame_inputs = tk.Frame(frame_bottom_left, bd=5, relief='ridge')
+frame_inputs.place(relx=0, rely=0, relwidth=1, relheight=1)
 frame_inputs.update()
 
-frame_table = tk.Frame(frame_bottom, bd=5, relief='ridge')
-frame_table.place(relx=0.25, rely=0, relwidth=0.75, relheight=1)
+frame_table = tk.Frame(frame_bottom_right_left_top, bd=5, relief='ridge')
+frame_table.place(relx=0, rely=0, relwidth=1, relheight=1)
 frame_table.update()
 
-frame_camera = tk.Frame(janela, bd=5, relief='ridge', width=int(janel_width/2), height=janela_height/2)
-frame_camera.place(x=int(janel_width/2), y=0)
-frame_camera.update()
-
-label = tk.Label(frame_camera, width=frame_camera.winfo_width(), height=frame_camera.winfo_height())
-label.pack()
-label.update()
-
-camera = Camera(database, label)
-label.after(0, camera.show_frame)
-
-frame_grafico = tk.Frame(janela, bd=5, relief='ridge', width=int(janel_width/2), height=janela_height/2)
-frame_grafico.place(x=0, y=0)
+frame_grafico = tk.Frame(janela, bd=5, relief='ridge')
+frame_grafico.place(x=0, y=0, relwidth=.5, relheight=.5)
 frame_grafico.update()
 
 fig = Figure(figsize=((janel_width/2)/100, (janela_height/2)/100), dpi=100)
@@ -59,28 +73,28 @@ passage_num_plot(database, canvas, ax, '')
 
 input_data = tkcalendar.DateEntry(frame_inputs, state='normal')
 input_data.delete(0, "end")
-input_data.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
+input_data.place(relx=0.05, rely=0.1, relwidth=0.9, relheight=0.1)
 
 button = tk.Button(frame_inputs, text="Dia específico", command=lambda:passage_num_plot(database, canvas, ax, input_data.get_date()))
-button.place(relx=0.1, rely=0.2, relwidth=0.4, relheight=0.1)
+button.place(relx=0.05, rely=0.2, relwidth=0.45, relheight=0.1)
 button = tk.Button(frame_inputs, text="Todos os dias", command=lambda:passage_num_plot(database, canvas, ax, ''))
-button.place(relx=0.5, rely=0.2, relwidth=0.4, relheight=0.1)
+button.place(relx=0.5, rely=0.2, relwidth=0.45, relheight=0.1)
 
 table = ttk.Treeview(frame_table)
 table['columns'] = ("apartamento", "responsavel", "placa")
 
 table.column("#0", width = 0)
-table.column("apartamento", anchor="w", stretch=1)
-table.column("responsavel", anchor="w", stretch=1)
-table.column("placa", anchor="w", stretch=1)
+table.column("apartamento", anchor="w", stretch=0, width = int(frame_bottom_right_left_top.winfo_width()/3))
+table.column("responsavel", anchor="w", stretch=0, width = int(frame_bottom_right_left_top.winfo_width()/3))
+table.column("placa", anchor="w", stretch=0, width = int(frame_bottom_right_left_top.winfo_width()/3))
 
 table.heading("apartamento",text="Apartamento", anchor="w")
 table.heading("responsavel",text="Responsável", anchor="w")
 table.heading("placa",text="Placa", anchor="w")
 
-table.place(relx = 0, rely = .0125, relheight=1, relwidth=1)
+table.place(relx = 0, rely = 0, relheight=1, relwidth=1)
 table_rows = database.get_table_columns()
-print(table_rows)
+
 for id,(apto, apto_info) in enumerate(table_rows.items()):
     if len(apto_info['moradores']) > 1:
         table.insert(parent='', index='end', iid=id, values=(apto, apto_info['responsavel'], apto_info['placa']))
@@ -89,6 +103,101 @@ for id,(apto, apto_info) in enumerate(table_rows.items()):
                 table.insert(parent=id, index='end', iid=f"c{id2}", values=('', morador, ''))
     else:
         table.insert(parent='', index='end', iid=id, values=(apto, apto_info['responsavel'], apto_info['placa']))
+
+apartaments = [1,2,3,4,5,6,7,8,9,10]
+apartament_selected = tk.StringVar()
+apartament_selected.set(apartaments[0])
+apartaments_dropdownbox = ttk.Combobox(frame_bottom_right_left_bottom,
+                                         textvariable= apartament_selected,
+                                         values=apartaments,
+                                         state="readonly")
+apartaments_dropdownbox.place(relx=.15,rely=0)
+apartaments_dropdownbox.update()
+
+pessoas = ["Nícholas","Vinícius","Henry"]
+pessoa_selected = tk.StringVar()
+pessoa_selected.set(pessoas[0])
+pessoas_dropdownbox = ttk.Combobox(frame_bottom_right_left_bottom,
+                                         textvariable= pessoa_selected,
+                                         values=pessoas,
+                                         state="readonly")
+pessoas_dropdownbox.place(relx=.55, rely=0)
+pessoas_dropdownbox.update()
+
+update_apartament = tk.Button(frame_bottom_right_left_bottom, text="Atualizar apartamento",
+                              command=lambda:database.update_apartament(apartament_selected.get(),pessoa_selected.get()))
+update_apartament.place(relx=.35, rely=pessoas_dropdownbox.winfo_height()/frame_bottom_right_left_bottom.winfo_height(),
+                        width=pessoas_dropdownbox.winfo_width())
+update_apartament.update()
+
+table_pessoas = ttk.Treeview(frame_bottom_right_right_top)
+table_pessoas['columns'] = ("nome", "apartamento", "data_nascimento", "status_morador")
+table_pessoas['show'] = 'headings'
+
+table_pessoas.column("nome", anchor="w", stretch=0, width = int(frame_bottom_right_left_top.winfo_width()*5/18))
+table_pessoas.column("apartamento", anchor="w", stretch=0, width = int(frame_bottom_right_left_top.winfo_width()*1/6))
+table_pessoas.column("data_nascimento", anchor="w", stretch=0, width = int(frame_bottom_right_left_top.winfo_width()*5/18))
+table_pessoas.column("status_morador", anchor="w", stretch=0, width = int(frame_bottom_right_left_top.winfo_width()*5/18))
+
+table_pessoas.heading("nome",text="Nome", anchor="w")
+table_pessoas.heading("apartamento",text="Apto", anchor="w")
+table_pessoas.heading("data_nascimento",text="Data de nascimento", anchor="w")
+table_pessoas.heading("status_morador",text="Status morador", anchor="w")
+
+table_pessoas.place(relx = 0, rely = 0, relheight=1, relwidth=1)
+table_pessoas_rows = database.get_pessoas_columns()
+
+for id,(pessoa, pessoa_info) in enumerate(table_pessoas_rows.items()):   
+    table_pessoas.insert(parent='', index='end', iid=id, values=(pessoa, pessoa_info['apartamento'], pessoa_info['data_nascimento'], pessoa_info['tipo_pessoa']))
+
+nome_pessoa_label = tk.Label(frame_bottom_right_right_bottom, text="Nome")
+nome_pessoa_label.place(relx=0, rely=0, relwidth=0.2, relheight=0.15)
+nome_pessoa = tk.Entry(frame_bottom_right_right_bottom)
+nome_pessoa.place(relx=.2, rely=0, relwidth=.3, relheight=0.15)
+
+apartament_pessoa_label = tk.Label(frame_bottom_right_right_bottom, text="Apartamento")
+apartament_pessoa_label.place(relx=0, rely=.15, relwidth=0.2, relheight=0.15)
+apartament_pessoa_selected = tk.StringVar()
+apartament_pessoa_selected.set(apartaments[0])
+apartaments_pessoa_dropdownbox = ttk.Combobox(frame_bottom_right_right_bottom,
+                                         textvariable= apartament_pessoa_selected,
+                                         values=apartaments,
+                                         state="readonly")
+apartaments_pessoa_dropdownbox.place(relx=.2, rely=.15, relwidth=.3, relheight=0.15)
+apartaments_pessoa_dropdownbox.update()
+
+nascimento_pessoa_label = tk.Label(frame_bottom_right_right_bottom, text="Nascimento")
+nascimento_pessoa_label.place(relx=.5, rely=0, relwidth=0.2, relheight=0.15)
+nascimento = tkcalendar.DateEntry(frame_bottom_right_right_bottom, state='normal')
+nascimento.place(relx=0.7, rely=0, relwidth=.3, relheight=0.15)
+
+tipo_pessoa_label = tk.Label(frame_bottom_right_right_bottom, text="Status")
+tipo_pessoa_label.place(relx=.5, rely=.15, relwidth=0.2, relheight=0.15)
+tipo_pessoa = ["Morador","Responsável"]
+pessoa_selected = tk.StringVar()
+pessoa_selected.set(tipo_pessoa[0])
+tipo_pessoa_dropdownbox = ttk.Combobox(frame_bottom_right_right_bottom,
+                                         textvariable= pessoa_selected,
+                                         values=tipo_pessoa,
+                                         state="readonly")
+tipo_pessoa_dropdownbox.place(relx=.7, rely=.15, relwidth=.3, relheight=0.15)
+tipo_pessoa_dropdownbox.update()
+
+create_pessoa = tk.Button(frame_bottom_right_right_bottom, text="Cadastrar pessoa",
+                              command=lambda:database.create_pessoa(nome_pessoa.get(), apartament_pessoa_selected.get(), nascimento.get_date(), pessoa_selected.get()  ))
+create_pessoa.place(relx=.25, rely=.6, relwidth=.5, relheight=0.15)
+create_pessoa.update()
+
+frame_camera = tk.Frame(janela, bd=5, relief='ridge')
+frame_camera.place(relx=.5, rely=0, relwidth=0.5, relheight=0.5)
+frame_camera.update()
+
+label = tk.Label(frame_camera, width=frame_camera.winfo_width(), height=frame_camera.winfo_height())
+label.place(relx=0, rely=0, relwidth=1, relheight=1)
+label.update()
+
+camera = Camera(database, label)
+label.after(0, camera.show_frame)
 
 janela.minsize(janela_height, janel_width)
 janela.mainloop()
